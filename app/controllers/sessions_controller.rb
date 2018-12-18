5001875f47cb0ed21f6a7bc440070f0d16833f17
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-18 23:28:31 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+<<<<<<< HEAD
     if !params[:name].nil? && !params[:name].blank?
       session[:name] = params[:name]
       redirect_to '/'
     else
       redirect_to action: 'new'
+=======
+    if params[:name].nil? || params[:name].blank?
+      redirect_to action: 'new'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+>>>>>>> f1f18cce4ef1d7c759db2845dc1e6483db82c4d3
     end
   end
 
--- spec/views/new.html.erb_spec.rb
+++ spec/views/new.html.erb_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
+<<<<<<< HEAD
 RSpec.describe "application/hello", type: :view do
+=======
+RSpec.describe "sessions/new.html.erb", type: :view do
+>>>>>>> f1f18cce4ef1d7c759db2845dc1e6483db82c4d3
 
   it "shows login link if not logged in" do
     session[:name] = nil
@@ -9,8 +13,16 @@ RSpec.describe "application/hello", type: :view do
   end
 
   it "greets user by name if logged in" do
+<<<<<<< HEAD
     session[:name] = 'Kitty'
     render
     expect(rendered).to include 'Hi, Kitty'
   end
+=======
+    session[:name] = "Kitty"
+    render
+    expect(rendered).to include "Hi, Kitty."
+  end
+
+>>>>>>> f1f18cce4ef1d7c759db2845dc1e6483db82c4d3
 end
